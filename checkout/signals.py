babc4ca8ccf0ae97b6734f74b79cc1f73e9cee3e
seanjcarley <seanjcarley@outlook.com>
2020-06-22 21:09:42 +0100

    added forms, admin, and signals

---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """ update order total on line item update or create """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """ update order total on line item delete """
+    instance.order.update_total()
+ 
