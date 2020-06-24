f2f5ae8d2354212569454bfff6d8fc397d978ea0
seanjcarley <seanjcarley@outlook.com>
2020-06-24 21:12:36 +0100

    added class methods to webhook handler and webhook view

---
+++ checkout/webhooks.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.http import HttpResponse
+from checkout.webhook_handler import StripeWH_Handler
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+import stripe
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """ listen for webhooks from stripe """
+    # setup
+    wh_secret = settings.STRIPW_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # get te webhook
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
+
+    print('Success!')
+    return HttpResponse(status=200)
