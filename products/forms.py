62b2694fe9f64b05fa38727ff8397059ef282e72
Sean <seanjcarley@outlook.com>
2020-07-07 17:30:48 +0100

    added product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import Product, Category
 
+
 class ProductForm(forms.ModelForm):
 
     class Meta:
-        model: Product
+        model = Product
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
@@ -15,4 +16,3 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
-            
