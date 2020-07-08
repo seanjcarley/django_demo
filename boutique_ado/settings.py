38e95eff3ac7bf6121e8cd37c33abd194bd731b0
Sean <seanjcarley@outlook.com>
2020-07-08 20:04:39 +0100

    added aws details

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     # other
     'crispy_forms',
+    'storages,'
 ]
 
 MIDDLEWARE = [
@@ -173,6 +174,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'django-ecomms-demo'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
 
 # stripe
 FREE_DELIVERY_THRESHOLD = 50
